039a34fadc4601eb73cb7ffec519bbbb928d98f3
Andrew <andrew@nordicgamelab.org>
2024-08-24 15:11:22 -0400

    Update sdk.test.js

--- tests/sdk.test.js
+++ tests/sdk.test.js
@@ -1,3 +1,7 @@
+/** 
+* @license Copyright (c) 2024 Nordic Game Lab LLC
+* You may not use this code without the express permission of Nordic Game Lab LLC in writing.
+*/
 import { test, expect } from '@playwright/test';
 import express from "express";
 import path from 'node:path'

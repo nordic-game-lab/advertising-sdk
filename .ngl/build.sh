1cf879d6658a76ffa150c1420ced84de5d31902d
Andrew <okbomer999@gmail.com>
2025-05-19 19:14:53 -0400

    update builder

---
+++ .ngl/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o pipefail
+
+cd ..
+echo "Installing dependencies"
+npm i
+echo "Linting code"
+npm run lint
+echo "Running builder"
+npx ngl-build build web
+
+echo "Adding license to final code"
+sed -i "1i/**\n*@license MPL 2.0\n*This Code Form is subject to the terms of the Mozilla Public\n*License, v. 2.0. If a copy of the MPL was not distributed with this\n*file, You can obtain one at https://mozilla.org/MPL/2.0/.\n*/" dist/ads.min.js
